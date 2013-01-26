80dad9b1f7508ca70f10ca1ccb822e4564cb185d
Gregory Goltsov <gregory@goltsov.info>
2013-01-26 10:54:22 +0000

    Mouse to world coordinate spawner done

--- Assets/scripts/PlayerScript.cs
+++ Assets/scripts/PlayerScript.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class PlayerScript : MonoBehaviour {
 
 	public float speed = 3.0F;
+	public Transform bullet;
 
 	private CharacterController controller;
 
@@ -15,6 +16,32 @@ public class PlayerScript : MonoBehaviour {
 		moveDirection = transform.TransformDirection(moveDirection);
 		moveDirection *= speed;
 		controller.Move(moveDirection * Time.deltaTime);
+
+		// Now you can simply pass the mouse cursor’s current position to get its equivalent world-space position like this:
+		if(Input.GetButtonDown("Fire1"))
+		{
+ 			Vector3 planePosition = ScreenToWorld(Input.mousePosition);
+			Instantiate(bullet, planePosition, Quaternion.identity);
+		}
+	}
+
+	// Borrowed from http://www.fatalfrog.com/?p=7
+	private Vector3 ScreenToWorld(Vector2 screenPos) {
+	    // Create a ray going into the scene starting
+	    // from the screen position provided
+	    Ray ray = Camera.main.ScreenPointToRay(screenPos);
+
+	    // ray hit an object, return intersection point
+	    // HOWEVER we're not looking for a collision
+	    // RaycastHit hit;
+	    // if( Physics.Raycast(ray, out hit ))
+	    //    return hit.point;
+
+	    // ray didn't hit any solid object, so return the
+	    // intersection point between the ray and
+	    // the Y=0 plane (horizontal plane)
+	    float t = -ray.origin.y / ray.direction.y;
+	    return ray.GetPoint(t);
 	}
 
 }
