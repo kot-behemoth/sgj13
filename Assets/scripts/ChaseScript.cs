3d12cf1ba9c599ce3e66b775b1728c328ac0537a
jonbro <jonbro@gmail.com>
2013-01-26 18:30:46 +0000

    added support for going to the nearest player in the chase script

--- Assets/scripts/ChaseScript.cs
+++ Assets/scripts/ChaseScript.cs
@@ -6,7 +6,8 @@ using System.Collections;
 public class ChaseScript : MonoBehaviour {
 	public GameObject target;
 	public float speed;
-
+	public GameObject[] players;
+	
 	// Use this for initialization
 	void Start () {
 		
@@ -14,6 +15,17 @@ public class ChaseScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		transform.position += (target.transform.position-transform.position).normalized*speed;
+		GameObject closestPlayer = players[0];
+		float distance = Vector3.Distance(players[0].transform.position, transform.position);
+		for(int i=1;i<players.Length;i++){
+			if(Vector3.Distance(players[i].transform.position, transform.position) < distance){
+				distance = Vector3.Distance(players[i].transform.position, transform.position);
+				closestPlayer = players[i];
+			}
+		}
+		// if towards
+		transform.position += (closestPlayer.transform.position-transform.position).normalized*speed;
+		// if away
+		//transform.position -= (closestPlayer.transform.position-transform.position).normalized*speed;
 	}
 }
